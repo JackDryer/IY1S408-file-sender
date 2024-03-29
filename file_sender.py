f31cf86c5c4b0ceae3b822b33ee8e101288accb9
Jack Dryer <jackjbld@gmail.com>
2024-03-29 17:00:21 +0000

    pep8 formated

--- file_sender.py
+++ file_sender.py
@@ -1,3 +1,4 @@
+import logging
 from pathlib import Path
 from typing import List
 from flask import Flask, send_from_directory, request, redirect, url_for
@@ -5,20 +6,22 @@ from werkzeug.utils import safe_join, secure_filename
 import os
 
 app: Flask = Flask(__name__)
-import logging
 log = logging.getLogger('werkzeug')
 
 DIR_PATH: Path = Path.cwd()
 
-toflash :List[str] = [] # how to avoid template injection, dont use templates, 
-#however this should never contain user input due to xss
-def get_flash() ->str:
+toflash: List[str] = []  # how to avoid template injection, dont use templates,
+# however this should never contain user input due to xss
+
+
+def get_flash() -> str:
     temp = ""
     while toflash:
-        temp+= toflash.pop()
+        temp += toflash.pop()
     return temp
 
-upload_form =   """
+
+upload_form = """
 <form method=post action="/upload" enctype=multipart/form-data>
       <input type=file name=file id=fileholder>
       <input type=submit value=Upload>
@@ -26,8 +29,9 @@ upload_form =   """
 
     """
 
+
 def get_files_from_directory(path):
-    path = Path(safe_join(DIR_PATH,path))
+    path = Path(safe_join(DIR_PATH, path))
     for dir_item in path.iterdir():
         if dir_item.is_file():
             yield "file", dir_item.name
@@ -36,33 +40,40 @@ def get_files_from_directory(path):
         else:
             raise FileExistsError
 
+
 @app.route("/files/<path:file_name>")  # type: ignore
 def serve_file(file_name: str):
     return send_from_directory(DIR_PATH, file_name)
 
 
-def html_ul_of_items(path:str) -> str:
+def html_ul_of_items(path: str) -> str:
     html: str = "<ul>"
     for item_type, dir_item in get_files_from_directory(path):
         if item_type == "file":
-            html += f"<li><a href='/files/{path}/{dir_item}'>{dir_item}</a></li>"
+            html += f"<li><a href='/files/{path}/{
+                dir_item}'>{dir_item}</a></li>"
         if item_type == "dir":
-            html += f"<li><a href='/explore/{path}/{dir_item}'>{dir_item}</a></li>"
+            html += f"<li><a href='/explore/{path}/{
+                dir_item}'>{dir_item}</a></li>"
     return f"{html}</ul>"
 
 
 @app.route("/")  # type: ignore
 def serve_index() -> str:
-    log.setLevel(logging.ERROR) 
-    return  html_ul_of_items("")+upload_form+ get_flash()
+    log.setLevel(logging.ERROR)
+    return html_ul_of_items("")+upload_form + get_flash()
+
 
 @app.route("/explore/")
-@app.route("/explore") 
+@app.route("/explore")
 def redirect_to_main() -> str:
     return redirect("/")
-@app.route("/explore/<path:folder_path>") 
+
+
+@app.route("/explore/<path:folder_path>")
 def serve_path(folder_path: str) -> str:
-    return html_ul_of_items(folder_path) +upload_form + get_flash()
+    return html_ul_of_items(folder_path) + upload_form + get_flash()
+
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
@@ -80,28 +91,33 @@ def upload_file():
         if file:
             filename = secure_filename(file.filename)
             filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            choice = input (f'A user wants to send a file "{filename}" Accept? y/\u0332N')
+            choice = input(f'A user wants to send a file "{
+                           filename}" Accept? y/\u0332N')
             if choice.lower() == "y":
                 file.save(filepath)
-                print (f"Saving to {filepath}")
+                print(f"Saving to {filepath}")
                 toflash.append("File uploaded successfully")
             else:
                 toflash.append("File rejected")
             return redirect(request.host_url)
+
+
 def main() -> None:
     """Run the flask app."""
     import argparse
     parser = argparse.ArgumentParser(prog='File Sender',
-                    description='Sends and receives files form the current working directory')
-    parser.add_argument("-p",'--port', nargs=1, default=8080, type=int)
-    parser.add_argument("-r",'--receive_folder', nargs=1, default="received_files/", type=str)
+                                     description='Sends and receives files form the current working directory')
+    parser.add_argument("-p", '--port', nargs=1, default=8080, type=int)
+    parser.add_argument("-r", '--receive_folder', nargs=1,
+                        default="received_files/", type=str)
     args = parser.parse_args()
-    port = args.port[0] if isinstance(args.port,list) else args.port
-    UPLOAD_FOLDER:str = args.receive_folder[0] if isinstance(args.receive_folder,list) else args.receive_folder
+    port = args.port[0] if isinstance(args.port, list) else args.port
+    UPLOAD_FOLDER: str = args.receive_folder[0] if isinstance(
+        args.receive_folder, list) else args.receive_folder
     app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
     if not Path(UPLOAD_FOLDER).exists():
         Path(UPLOAD_FOLDER).mkdir()
-    app.run(host='0.0.0.0',port=port)
+    app.run(host='0.0.0.0', port=port)
 
 
 if __name__ == "__main__":
